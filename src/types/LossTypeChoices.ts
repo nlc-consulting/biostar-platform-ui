0b9e12b9efa4446077fc956f7c46c36a7f65b1ee
Nick Chiafullo <nick@nlc.consulting>
2026-01-15 23:33:05 -0500

    feat: add LOSS_TYPE_CHOICES for categorizing loss types

---
+++ src/types/LossTypeChoices.ts
@@ -0,0 +1,9 @@
+export const LOSS_TYPE_CHOICES = [
+  { id: 'Water', name: 'Water' },
+  { id: 'Mold', name: 'Mold' },
+  { id: 'Fire', name: 'Fire' },
+  { id: 'Biohazard', name: 'Biohazard' },
+  { id: 'Storm', name: 'Storm' },
+  { id: 'Reconstruction', name: 'Reconstruction' },
+  { id: 'Contents', name: 'Contents' }
+];
