e3ac5bc23914f4311653968fe864f394c65891c4
Nick Chiafullo <nick@nlc.consulting>
2026-01-28 23:51:43 -0500

    feat: add color-coded Loss Type display in ProjectList, RecentLeadsReport, and RecentProjectsReport

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
-import { Datagrid, DateField, EditButton, List, ShowButton, TextField, TextInput } from 'react-admin';
-import { Container } from '@mui/material';
+import {
+  Datagrid,
+  DateField,
+  EditButton,
+  List,
+  ShowButton,
+  FunctionField,
+  TextField,
+  TextInput,
+  SelectInput
+} from 'react-admin';
+import { Chip, Container } from '@mui/material';
 import BreadcrumbsNav from '../components/BreadcrumbsNav.tsx';
+import { LOSS_TYPE_CHOICES } from '../types/LossTypeChoices.ts';
+import { getLossTypeColor } from '../types/LossTypeColors.ts';
 
-const projectFilters = [<TextInput source="q" label="Search" alwaysOn key="q" />];
+const projectFilters = [
+  <TextInput source="q" label="Search" alwaysOn key="q" />,
+  <SelectInput source="lossType" label="Department" choices={LOSS_TYPE_CHOICES} alwaysOn key="lossType" />
+];
+
 
 const ProjectList: React.FC = () => {
   return (
@@ -15,12 +31,27 @@ const ProjectList: React.FC = () => {
         ]}
       />
       <List filters={projectFilters} sort={{ field: 'createdAt', order: 'DESC' }}>
-        <Datagrid rowClick="edit" bulkActionButtons={false}>
+        <Datagrid rowClick="show" bulkActionButtons={false}>
           <TextField source="name" label="Project" />
           <TextField source="status" label="Status" />
           <TextField source="customer.firstName" label="Customer First" />
           <TextField source="customer.lastName" label="Customer Last" />
-          <TextField source="lossType" label="Loss Type" />
+          <FunctionField
+            label="Loss Type"
+            render={(record) => {
+              const value = record?.lossType ?? '';
+              const color = getLossTypeColor(value);
+              return value ? (
+                <Chip
+                  size="small"
+                  label={value}
+                  sx={{ bgcolor: color, color: '#fff' }}
+                />
+              ) : (
+                '-'
+              );
+            }}
+          />
           <TextField source="propertyCity" label="City" />
           <DateField source="createdAt" label="Created" />
           <ShowButton />
